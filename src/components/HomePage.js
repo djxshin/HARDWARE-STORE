e846b6737ffb377effec9458383a4b3fdb75dff3
DJ Shin <djatlienriseup@gmail.com>
2019-03-06 18:58:12 -0500

    2nd commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Hardware Store</h1>
+	      <div>Currently On Sale: A Hammer!</div>
+      </div>
+    )
+  }
+}

bb887d49d88afb855aa7dfb319acaf4638841e9f
DJ Shin <djatlienriseup@gmail.com>
2019-03-06 22:58:39 -0500

    little css added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 export default class HomePage extends Component {
 
   state = {
@@ -8,17 +9,27 @@ export default class HomePage extends Component {
   }
 
   handleClick = () => {
-    console.log('hey man!!')
+    console.log('i was clicked')
+    this.setState({
+      showEditForm: !this.state.showEditForm
+    })
+   
   }
 
   render() {
 
     return (
       <div>
-        <div>
+        <div className="hardware-header">
         <h1>My Hardware Store</h1>
 	      <div>Currently On Sale: {this.state.itemCurrentlyOnSale} !</div>
-        <button onClick={this.handleClick}>Edit Sale Item</button>
+        <button onClick={this.handleClick}>
+        { this.state.showEditForm
+          ? "Hide"
+          : "Edit Sale Item"
+      
+        }
+        </button>
           {this.state.showEditForm ? (
               <div>
                 <input type="text" placeholder="Update Item Here"/>

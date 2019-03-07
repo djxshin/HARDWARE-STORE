967c6dec60cc84aa3ee54fea2f1317e95ec9e285
DJ Shin <djatlienriseup@gmail.com>
2019-03-06 20:10:00 -0500

    turnary

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,20 +3,26 @@ import React, { Component } from 'react'
 export default class HomePage extends Component {
 
   state = {
-    itemCurrentlyOnSale: 'some nails'
+    itemCurrentlyOnSale: 'some nails',
+    showEditForm: false
   }
 
   render() {
+
     return (
       <div>
+        <div>
         <h1>My Hardware Store</h1>
 	      <div>Currently On Sale: {this.state.itemCurrentlyOnSale} !</div>
         <button>Edit Sale Item</button>
-
-        <div>
-          <input type="text" placeholder="Update Item Here"/>
-        </div>
-
+          {this.state.showEditForm ? (
+              <div>
+                <input type="text" placeholder="Update Item Here"/>
+              </div>
+            ): null
+            }
+        
+          </div>
       </div>
     )
   }

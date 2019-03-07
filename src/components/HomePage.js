38fc5cf6a3686d527fcd50d5a6f59c42a89c346f
DJ Shin <djatlienriseup@gmail.com>
2019-03-06 19:38:09 -0500

    added state on homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 export default class HomePage extends Component {
+
+  state = {
+    itemCurrentlyOnSale: 'some nails'
+  }
+
   render() {
     return (
       <div>
         <h1>My Hardware Store</h1>
-	      <div>Currently On Sale: A Hammer!</div>
+	      <div>Currently On Sale: {this.state.itemCurrentlyOnSale} </div>
       </div>
     )
   }
